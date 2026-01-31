19cf2390dabb2fe15c16103b30d6c0be573371ca
rotchana10 <saow102547@gmail.com>
2026-01-31 14:14:30 +0700

    Collection of python

---
+++ python2.py
@@ -0,0 +1,46 @@
+# Pate: Python  Operators
+# Arithmetic Operators
+
+"""""
+x = 4 + 5  # Addition
+y = 10 - 2 # Subtraction
+z = 3 * 7 # Multiplication
+a = 8 / 2 # Division
+b = 10 % 3 # Modulus
+c = 2 ** 3 # Exponentiation
+
+
+x = 5
+x = x + 3
+x += 3  # x = x + 3
+x = x + 3
+"""
+
+# Pate: Python  Operators
+# Arithmetic Operators
+
+""""
+a = 5
+b = 10
+print (a == b)  # Equal to
+print (a != b)  # Not equal to
+print (a > b)  # Greater than
+print (a < b)  # Less than
+print (a >= b)  # Greater than or equal to
+print (a <= b)  # Less than or equal to
+"""
+
+# Pate: Python  Operators
+# Logical Operators   
+
+x = True
+y = False
+print (x and y)  # Logical AND จริงเเละจริง
+print (x or y)   # Logical OR   เป็นเท็จ ต้องเป็นเท็จทั้งคู่
+print (not x)    # Logical NOT
+print (x & y)  # Bitwise AND
+print (x | y)   # Bitwise OR
+print (~x)    # Bitwise NOT
+a = 5
+b = 10
+print ((a < 10) and (b > 5))  # True and True -> True
