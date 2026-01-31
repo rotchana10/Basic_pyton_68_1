27c3de4418d1b340ee2da1381ffd6d9953d0c83e
rotchana10 <saow102547@gmail.com>
2026-01-31 13:19:58 +0700

    python variables

---
+++ python1.py
@@ -0,0 +1,37 @@
+"""
+#
+# Pate: Python Comment
+#
+"""
+
+print("Hello,Wold")
+
+"""
+#
+# Part: Python variables
+#
+"""
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variables naming 
+#
+"""
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400 
+MYVAR = 500
+
+#
+myVariableName = 900
+MyVariableName = 1000
+my_variable_name = 1100
