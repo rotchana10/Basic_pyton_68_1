f0cfb3c0bf4e2245d712cbf671c60eff674bb16d
rotchana10 <saow102547@gmail.com>
2026-01-31 14:24:43 +0700

    conditional statements

---
+++ python4.py
@@ -0,0 +1,13 @@
+# conditional statements
+
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
